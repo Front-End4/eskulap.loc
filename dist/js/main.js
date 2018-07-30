9e522bff468466f774ea40a705b4b123b0f5062d
Alexander Vasuluev <alexhateful@gmail.com>
2018-07-30 19:32:21 +0300

    add scroll

--- dist/js/main.js
+++ dist/js/main.js
@@ -10,3 +10,34 @@
 // close.addEventListener('click', event => {
 //     popupMenu.classList.toggle('is-active');
 // });
+
+
+// $('.js-scroll').on('click', function (event) {
+//     event.preventDefault();
+//     const anchor = $(this).attr('href');
+//     $(window).animate({
+//         scrollTop: 0
+//         },
+//         '1000'
+//     )
+// })
+var inst = $('[data-remodal-id=menu]').remodal();
+
+var body = $('html, body');
+
+$('.js-menu').on('click', '.menu__anchor', function(e){
+    e.preventDefault();
+    inst.close();
+    const anchor = $(this).attr('href');
+    body.animate({
+        scrollTop: $(anchor)[0].offsetTop + 'px'
+    }, 500, 'swing');
+});
+
+$('.js-scroll').on('click', function(e){
+    e.preventDefault();
+    const anchor = $(this).attr('href');
+    body.animate({
+        scrollTop: $(anchor)[0].offsetTop + 'px'
+    }, 500, 'swing');
+});
